c7e174cdf03f1906ec77b41bd5433ea5663aca9c
mgracioli <michel-rg@hotmail.com>
2023-04-10 11:15:28 -0300

    Configurações dos arquivos iniciais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head';
 import React from 'react'
+import './styles/globals.css'
+import './styles/variables.css'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  const favicon = '/favicon.ico';
+
+  return (
+    <>
+      <Head>
+        <link rel='shortcut icon' href={favicon} />
+        <title>Projeto avaliação</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,69 +1,51 @@
 import React from 'react'
-import Head from 'next/head'
-import { NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
+import HomePage from './home'
+import { FeedbacksUsuarios, HomePageProps } from './home/@types/types'
 
-const Home: NextPage = () => {
+const IndexPage: NextPage = ({ feedbacksUsuarios }: HomePageProps) => {
   return (
-    <div >
-      <Head>
-        <title>Projeto Avaliacao</title>
-      </Head>
-
-      {/* <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer> */}
-    </div>
+    <HomePage feedbacksUsuarios={feedbacksUsuarios} />
   )
 }
 
-export default Home
+export default IndexPage
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  let props = {
+    feedbacksUsuarios: {} as Array<FeedbacksUsuarios>
+  }
+
+  //mock dos dados iniciais que seriam buscados na API
+  const arrayFeedbacks = [
+    {
+      id: 0,
+      nome: 'David',
+      sobrenome: 'Ramirez',
+      descricao: 'Os programas atendem todas as exigências que um Departamento Pessoal necessita. Na e-Contab eu encontrei a qualidade que a minha empresa precisa".',
+      linkFoto: 'assets/usuario0'
+    },
+    {
+      id: 1,
+      nome: 'David',
+      sobrenome: 'Ramirez',
+      descricao: 'Os programas atendem todas as exigências que um Departamento Pessoal necessita. Na e-Contab eu encontrei a qualidade que a minha empresa precisa".',
+      linkFoto: 'assets/usuario1'
+    },
+    {
+      id: 2,
+      nome: 'David',
+      sobrenome: 'Ramirez',
+      descricao: 'Os programas atendem todas as exigências que um Departamento Pessoal necessita. Na e-Contab eu encontrei a qualidade que a minha empresa precisa".',
+      linkFoto: 'assets/usuario2'
+    }
+  ]
+
+  props.feedbacksUsuarios = arrayFeedbacks
+
+  return {
+    props: {
+      feedbacksUsuarios: props.feedbacksUsuarios
+    }
+  };
+};
